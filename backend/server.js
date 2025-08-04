fa6c315bac6e77b5cf72833142c496e19bca4767
Rhogenn <91942812+ManaChanCoder@users.noreply.github.com>
2025-08-04 19:49:22 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+ 
     credentials: true,
   })
 );
