07188804b1b17fe7dc218c414a4cfb5ccded464b
Rhogenn <91942812+ManaChanCoder@users.noreply.github.com>
2025-08-22 16:51:27 +0800

    Update Project.jsx

--- frontend/mern-portfolio/src/components/pages/Project.jsx
+++ frontend/mern-portfolio/src/components/pages/Project.jsx
@@ -68,7 +68,7 @@ const Project = () => {
                 </span>
               </section>
 
-              <div className="flex justify-end gap-5 pr-3">
+              <div className="flex justify-end gap-5 pr-5">
   <div className="relative group">
     <a href={proj.url}>
       <FiActivity
@@ -79,7 +79,7 @@ const Project = () => {
     <span className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 
                      px-2 py-1 text-xs text-white bg-gray-800 rounded opacity-0 
                      group-hover:opacity-100 transition-opacity duration-300">
-      Visit Project
+     Demo
     </span>
   </div>
                 
